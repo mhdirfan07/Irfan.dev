ecd1d167373be806d6df4d2ca4ac04ad8b2de6f7
7crescentmoon <mirfan1537@gmail.com>
2026-05-23 18:09:55 +0700

    feat: configure static export, add Keystatic route, and implement GitHub Pages deployment workflow

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+
+  images: {
+    unoptimized: true,
+  },
   allowedDevOrigins: ['192.168.1.86'],
 };
 
--- src/app/keystatic/[[...params]]/page.tsx
+++ src/app/keystatic/[[...params]]/page.tsx
@@ -1,6 +1,8 @@
-"use client";
-
 import { makePage } from '@keystatic/next/ui/app';
 import config from '../../../../keystatic.config';
 
 export default makePage(config);
+
+export function generateStaticParams() {
+  return [];
+}
